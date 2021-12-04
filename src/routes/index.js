45132e06ff6664841aa7b2dbd55f0f2869f83cd0
Volper212 <volpergaming@gmail.com>
2021-12-04 14:34:41 +0100

    Try to fix again

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const server = new WebSocketServer({ port: 8080 });
 
 // Added after @bluwy's comment, still crashes
 if (import.meta.hot) {
+    import.meta.hot.accept();
     import.meta.hot.dispose(() => {
         server.close();
     });
